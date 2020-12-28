bc7fcf1774e79ee12d831176648b51b1c61e1199
Work <work@Xiaoleis-MacBook-Pro.local>
2020-12-28 11:57:37 -0800

    test

---
+++ test/pipe.groovy
@@ -0,0 +1,27 @@
+//def gitUrl = "https://github.com/example/project.git"
+
+pipelineJob("MyProject-Test") {
+        properties {
+        pipelineTriggers {
+            triggers {
+                pollSCM {
+                    scmpoll_spec('H/5 * * * *')
+                }
+            }
+        }
+    }
+    definition {
+        cpsScm {
+            lightweight()
+            scm {
+                git {
+                branch('origin/master')
+                remote {
+                    url('git@github.com:dajxennx/FisrtRepo.git')
+                }
+                }
+                scriptPath('test/Jenkinsfile')
+            }
+        }
+    }
+}
