d4760a1e405a0347580af02e21d5f4cc40a39fcd
Work <work@Xiaoleis-MacBook-Pro.local>
2021-01-14 20:42:11 -0800

    build after other projects are built

--- test/pipe.groovy
+++ test/pipe.groovy
@@ -24,6 +24,10 @@ pipelineJob("MyProject-Test") {
                 }
                 scriptPath('test/Jenkinsfile')
             }
+            triggers {
+            buildResult('H/* * * * *') {
+            upstream('MyProject-Build', 'UNSTABLE')
+        }
         }
     }
 }
